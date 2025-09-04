29a26acaab9e579ffff8568c8a9aafb2edffa624
owen-deepskill <owen.cho@deepskill.io>
2025-09-04 13:45:43 +0900

    test script

---
+++ test_script.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.metrics import confusion_matrix, accuracy_score, precision_score, recall_score
+
+print("테스트")
